9d1eff50f764d983c0be705df2922853d4fedd4f
Wei Jian <ngweijian@weis-mbp.home>
2020-08-10 17:31:00 +0100

    Removed not used dependency UseRef from status bar

--- src/components/StatusBar/StatusBar.js
+++ src/components/StatusBar/StatusBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { resetBoard, selectMode } from "../../actions";
 
